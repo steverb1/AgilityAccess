2022de24b89e0cf3fa40927b84b2b67da0d9f681
steverb1 <steverb@mindspring.com>
2025-06-06 10:28:46 -0600

    Expanded integration test to include getStoryHistories

--- src/integration-test/java/com/access/versionone/ActivityFetcherIT.java
+++ src/integration-test/java/com/access/versionone/ActivityFetcherIT.java
@@ -3,19 +3,35 @@ package com.access.versionone;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ActivityFetcherIT {
     @Test
-    void getTeamsToProcess() throws IOException, InterruptedException {
+    void fetchAllDataForScope() throws IOException, InterruptedException {
         ActivityFetcher activityFetcher = new ActivityFetcher(new HttpClientWrapper(),
                 "https://www16.v1host.com/api-examples",
                 PropertyFetcher.getProperty("v1.token"));
 
         Map<String, String> teamsToProcess = activityFetcher.getTeamsToProcess("Scope:1005", null);
+        assertThat(teamsToProcess).isNotEmpty();
 
-        assertThat(!teamsToProcess.isEmpty());
+        List<StoryHistory> storyHistories = activityFetcher.getStoryHistories(teamsToProcess, true, true);
+        assertThat(storyHistories).isNotEmpty();
+    }
+
+    @Test
+    void fetchAllDataForTeam() throws IOException, InterruptedException {
+        ActivityFetcher activityFetcher = new ActivityFetcher(new HttpClientWrapper(),
+                "https://www16.v1host.com/api-examples",
+                PropertyFetcher.getProperty("v1.token"));
+
+        Map<String, String> teamsToProcess = activityFetcher.getTeamsToProcess(null, "Team:1889");
+        assertThat(teamsToProcess).isNotEmpty();
+
+        List<StoryHistory> storyHistories = activityFetcher.getStoryHistories(teamsToProcess, true, true);
+        assertThat(storyHistories).isNotEmpty();
     }
 }
